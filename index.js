1a6165d6164a7a443877bbf84ec01c3636a70f08
Raja <>
2020-06-16 04:57:10 -0700

    Commit #2

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import {AppRegistry} from 'react-native';
 import RouterComponent from './components/Router';
 import {name as appName} from './app.json';
 
+console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => RouterComponent);
